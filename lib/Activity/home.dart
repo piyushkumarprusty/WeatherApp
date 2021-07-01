1ffffe57d161dac37365f46b034081163fe87674
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-01 23:07:06 +0530

    futuredelay

--- lib/Activity/home.dart
+++ lib/Activity/home.dart
@@ -6,10 +6,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String username = "Piyush";
+
+  void getData() {
+    //process
+    Future.delayed(Duration(seconds: 5), () {
+      print("$username ");
+    });
+  }
+
+  void showData() {
+    print("$username");
+  }
+
+  void initStatee() {
+    super.initState();
+    getData();
+    showData();
+    print("this is init state");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Activity"),
+      //body: Text("Home Activity"),
+
+      appBar: AppBar(
+        title: Text("Home Activity"),
+      ),
     );
   }
 }
